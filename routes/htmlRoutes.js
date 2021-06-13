d785d55bad7950a06e4a1ee3c7f98fad19b74ee6
Brandon Cowley <brandon.cowley@gmail.com>
2021-06-13 12:22:08 -0700

    Finally displaying notes! Route fixed to /notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const router = require('express').Router();
 
-// OOO, this router is first
+
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
